546b0397ea012339691d9de679a9871b46744e3a
itsyoboygod <m.vini.ac@hotmail.com>
2023-01-20 16:15:07 -0300

    stablishing communication between bckg and dom files

--- background.js
+++ background.js
@@ -15,9 +15,36 @@ chrome.action.onClicked.addListener((tab) => {
 
 chrome.runtime.onMessage.addListener(reciever)
 
-chrome.word = "word"
+let word = "word"
 
 function reciever(request, sender, sendResponse){
     console.log(request)
-    chrome.word = request.text
+    word = request.text
+    console.log("_______________")
+    console.log("word selected(bckg.js): ")
+    console.log(word + ' <- bckg.js')
 }
+
+
+
+
+// ----------------- COMMUNICATION BETWEEN BACKGROUND & ESPECIFIC PORT DOM File.JS  ---------------
+
+// opens a communication port
+chrome.runtime.onConnect.addListener(function(port) {
+
+    // listen for every message passing throw it
+    port.onMessage.addListener(function(o) {
+
+        // if the message comes from the popup
+        if (o.from && o.from === 'popup' && o.start && o.start === 'Y') {
+
+            // inserts a script into your tab content
+            chrome.tabs.executeScript(null, {
+
+                // the script will click the button into the tab content
+                code: "document.getElementById('pageBtn').click();"
+            });
+        }
+    });
+});
--- scripts/wordSelec.js
+++ scripts/wordSelec.js
@@ -2,15 +2,39 @@ console.log('Word selected running!')
 
 window.addEventListener('mouseup', wordSelected)
 
-function wordSelected(){
-    console.log("_______________")
-    console.log("word selected: ")
-    let selectedText = window.getSelection().toString().trim()
-    console.log(selectedText)
-        if(selectedText.length > 0){
-            let message = {
-                text: selectedText
-            }
-            chrome.runtime.sendMessage(message)
+        function wordSelected(){
+            var port = chrome.runtime.connect();
+            console.log("_______________")
+            console.log("word selected(wordsel.js): ")
+            let selectedText = window.getSelection().toString().trim()
+            console.log(selectedText)
+                if(selectedText.length > 0){
+                    let message = {
+                        text: selectedText
+                    }
+                    chrome.runtime.sendMessage(message)
+                    window.document.getElementById('test').innerHTML = message.text
+                    console.log(message.text + ' <- wordSel.js')
+                }
+        }
+
+
+
+    async function connectDOM(){
+            // always waits the document to be loaded when shown
+            document.addEventListener('DOMContentLoaded', await function() {
+
+                // opens a communication between scripts
+                var port = chrome.runtime.connect();
+
+                // listens to the click of the button into the popup content
+             document.getElementById('test').addEventListener('click', function() {
+
+                    // sends a message throw the communication port
+                  port.postMessage({
+                        'from': 'popup',
+                        'start': 'Y'
+                    });
+                });
+            });
     }
-}
