7e3c79fdaf37b684a10eec41bb82ba31ba4a1df8
itsyoboygod <m.vini.ac@hotmail.com>
2023-03-02 15:59:21 -0300

    modifying and improving layout

--- popup/rdm_clr.js
+++ popup/rdm_clr.js
@@ -1,8 +1,8 @@
             const id__report = document.getElementById("id__report")
-            const id__test = document.getElementById("test")
+            const id__veryfied = document.getElementById("veryfied")
     
             var colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
             var rando = () => colors[Math.floor(Math.random() * colors.length)]
             id__report?id__report.style.color = `${rando()}`:""
-            id__test?id__test.style.color = `${rando()}`:""
+            id__veryfied?id__veryfied.style.color = `${rando()}`:""
             // console.log(rando(colors[0]))
