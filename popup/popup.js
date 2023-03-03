c957df4a67ce50fdd7edac6fe48acbe607d198f6
itsyoboygod <m.vini.ac@hotmail.com>
2023-03-03 15:54:12 -0300

    li's styling || working on wordnik translate

--- popup/popup.js
+++ popup/popup.js
@@ -13,32 +13,31 @@ chrome.runtime.sendMessage({
   `);
 });
 
-function word() {
-  let bgpage = chrome.extension.getBackgroundPage();
-  let word = bgpage.word;
-  // createP(word)
+// function word() {
+//   let bgpage = chrome.extension.getBackgroundPage();
+//   let word = bgpage.word;
+//   // createP(word)
 
-  let url = `https://api.wordnik.com/v4/word.json/
-  ${word}
-  /definitions?limit=1
-  &includeRelated=false
-  &sourceDictionaries=all
-  &useCanonical=false
-  &includeTags=false
-  &api_key=6f9nbd1ye2vn1e15uu86gpuo2vg9mid0j8cq2ztfoguc3u3tf
-  `
-  url = url.replace(/\s+/g, '')
-  loadJSON(url, gotData)
+//   let url = `https://api.wordnik.com/v4/word.json/
+//   ${word}
+//   /definitions?limit=1
+//   &includeRelated=false
+//   &sourceDictionaries=all
+//   &useCanonical=false
+//   &includeTags=false
+//   &api_key=6f9nbd1ye2vn1e15uu86gpuo2vg9mid0j8cq2ztfoguc3u3tf
+//   `
+//   url = url.replace(/\s+/g, '')
+//   loadJSON(url, gotData)
 
-  function gotData(data){
-    createP(data[0].text)
-  }
+//   function gotData(data){
+//     createP(data[0].text)
+//   }
+// }
+  
+  var wordnik = document.getElementById("id__wordnik")
+  wordnik.innerText = "word()"
 
-  return "wefdewqfewfewf"
-}
-
-// var wordsel = document.getElementById("wordsel")
-// wordsel.innerText = word()
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.source === "wordselec.js") {
